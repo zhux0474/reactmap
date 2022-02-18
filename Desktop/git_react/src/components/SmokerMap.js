05ca46121fcc90baa2b1018c17fcb3c7cb7d4296
zhux0474 <zhux0474@umn.edu>
2022-02-18 10:23:15 -0600

    fixed height

--- Desktop/git_react/src/components/SmokerMap.js
+++ Desktop/git_react/src/components/SmokerMap.js
@@ -1,14 +1,56 @@
 import React from "react";
 import { fromLonLat, get } from "ol/proj";
 //import { Map, GeoJSON } from "react-leaflet";
-import{
-    interaction,layer,custom,control, //name spaces
-    Interactions,Overlays,Controls, //group
-    Map,Layers,Overlay,Util //objects
-  } from 'react-openlayers';
-//import "leaflet/dist/leaflet.css";
-import "./SmokerMap.css";
 
+//import "leaflet/dist/leaflet.css";
+//import "./SmokerMap.css";
+import Map from "ol/Map";
+//import Feature from "ol/Feature";
+import View from "ol/View";
+//import { Point, Style, Circle, Fill } from "ol";
+import TileLayer from "ol/layer/Tile";
+//import VectorLayer from "ol/layer/Vector";
+//import VectorSource from "ol/source/Vector";
+import OSM from "ol/source/OSM";
+
+
+export default class SmokerMap extends React.Component{
+  constructor(props){
+    super(props);
+    this.mapRef=React.createRef();
+  }
+  componentDidMount(){
+    this.map=new Map({
+      target: "mapContainer",
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view :new View({
+        center:fromLonLat([-94.6859,46.7296]),
+        zoom: 6
+      })
+
+    });
+   
+  }
+  render() {
+    console.log("-> render App");
+    return (
+      <div
+        id="mapContainer"
+        ref={this.mapRef}
+        style={{ width: "100%", height: "500px" }}
+      >
+       
+      </div>
+    );
+  }
+
+
+}
+/*
 const SmokerMap = ({ counties}) =>{
     /*
     const mapStyle={
@@ -32,7 +74,7 @@ const SmokerMap = ({ counties}) =>{
             onEachFeature={onEachCountry}
           />
         </Map>
-      );*/
+      );
 
       return (
         <div className="map_div">
@@ -48,4 +90,4 @@ const SmokerMap = ({ counties}) =>{
 
 };
 export default SmokerMap;
-
+*/
